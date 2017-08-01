bfc4009779be9402a448a498cc413ac34d12bdeb
Chandrakant <chandrakant@logistimo.com>
2017-08-01 12:30:03 +0530

    docker entry-point changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,9 +7,9 @@ spring.data.mongodb.database=$CALLISTO_DATABASE"
 
 exec java -cp $TOMCAT_HOME/webapps/callisto-application-0.1.0-SNAPSHOT.jar \
 	-Dloader.path=$TOMCAT_HOME/lib/callisto-plugins-functions-0.1.0-SNAPSHOT.jar \
-	-Dloader.main=com.logistimo.callisto.CallistoApplication org.springframework.boot.loader.PropertiesLauncher \
 	-Dserver.port=$SERVER_PORT \
 	-Dspring.data.mongodb.host=$MONGODB_HOST \
 	-Dspring.data.mongodb.port=$MONGODB_PORT \
 	-javaagent:$TOMCAT_HOME/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:$TOMCAT_HOME/jmx_exporter.json \
-	-Dspring.data.mongodb.database=$CALLISTO_DATABASE
+	-Dspring.data.mongodb.database=$CALLISTO_DATABASE \
+	-Dloader.main=com.logistimo.callisto.CallistoApplication org.springframework.boot.loader.PropertiesLauncher
