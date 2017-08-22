bad36ef841b0d1355dc87ed702c83aaa8175bde3
Chandrakant <chandrakant@logistimo.com>
2017-08-22 10:34:49 +0530

    topx bottomx comparator

--- modules/callisto-application/src/main/java/com/logistimo/callisto/CallistoApplication.java
+++ modules/callisto-application/src/main/java/com/logistimo/callisto/CallistoApplication.java
@@ -27,7 +27,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.logistimo.*")
 @EnableCaching
 public class CallistoApplication {
   public static void main(String[] args) {
