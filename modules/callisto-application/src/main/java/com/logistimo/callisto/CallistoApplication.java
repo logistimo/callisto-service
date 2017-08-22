f803bace7c62251c77f1f67ffc39e8cbd552e2ee
Chandrakant <chandrakant@logistimo.com>
2017-08-22 10:57:17 +0530

    scanBasePackage removed

--- modules/callisto-application/src/main/java/com/logistimo/callisto/CallistoApplication.java
+++ modules/callisto-application/src/main/java/com/logistimo/callisto/CallistoApplication.java
@@ -27,7 +27,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
-@SpringBootApplication(scanBasePackages = "com.logistimo.*")
+@SpringBootApplication
 @EnableCaching
 public class CallistoApplication {
   public static void main(String[] args) {
