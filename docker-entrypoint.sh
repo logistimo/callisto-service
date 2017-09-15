dc4fbbdac6e7271646c8f5f99f965195ea2a27e1
Charan Malemarpuram <charan@logistimo.com>
2017-09-15 12:51:12 +0530

    Remove dependency on logistimo callisto functions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,11 +5,11 @@ spring.data.mongodb.host=$MONGODB_HOST
 spring.data.mongodb.port=$MONGODB_PORT
 spring.data.mongodb.database=$CALLISTO_DATABASE"
 
-exec java -cp $TOMCAT_HOME/webapps/*.jar \
-	-Dloader.path=$TOMCAT_HOME/lib/* \
+exec java -cp $CALLISTO_HOME/*.jar \
+	-Dloader.path=$CALLISTO_HOME/lib/* \
 	-Dserver.port=$SERVER_PORT \
 	-Dspring.data.mongodb.host=$MONGODB_HOST \
 	-Dspring.data.mongodb.port=$MONGODB_PORT \
-	-javaagent:$TOMCAT_HOME/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:$TOMCAT_HOME/jmx_exporter.json \
+	-javaagent:$CALLISTO_HOME/jmx_prometheus_javaagent-0.7.jar=$JAVA_AGENT_PORT:$CALLISTO_HOME/jmx_exporter.json \
 	-Dspring.data.mongodb.database=$CALLISTO_DATABASE \
 	-Dloader.main=com.logistimo.callisto.CallistoApplication org.springframework.boot.loader.PropertiesLauncher
