30c65e81d42801570ea31f91f7c8ccf77afe54e1
Chandrakant <chandrakant@logistimo.com>
2018-01-22 10:43:53 +0530

    save query button

--- modules/callisto-frontend/app/demo/src/app/app.component.ts
+++ modules/callisto-frontend/app/demo/src/app/app.component.ts
@@ -10,105 +10,106 @@ declare var jQuery:any;
 declare var $:any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [DataService]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    providers: [DataService]
 })
 export class AppComponent {
-  title = 'app';
-  queryModel : string = '';
-  colsModel : string = '';
-  dbs = [];
-  dbsModel = {id: 1};
-  private el: ElementRef;
+    title = 'app';
+    queryModel:string = '';
+    colsModel:string = '';
+    dbs = [];
+    dbsModel = {id: 1};
+    private el:ElementRef;
 
-  constructor(private dataService : DataService, @Inject(ElementRef)el: ElementRef, public dialog: MatDialog){
-    this.el = el;
-  }
-
-  ngOnInit() {
-
-    this.dataService.getUser().subscribe(data => {
-      var _dbs = this.dbs;
-      var _dbsModel = this.dbsModel;
-      data['server_configs'].forEach(function(server){
-        _dbs.push(server);
-        if(_dbsModel['id'] == undefined){
-          _dbsModel = server;
-        }
-      });
-      console.log(data);
-    });
-
-  }
-
-  renderTable(input){
-
-    if (!this.el ||
-        !this.el.nativeElement ||
-        !this.el.nativeElement.children){
-      console.log('cant build without element');
-      return;
+    constructor(private dataService:DataService, @Inject(ElementRef)el:ElementRef, public dialog:MatDialog) {
+        this.el = el;
     }
 
-    var container = this.el.nativeElement;
-    var inst = jQuery(container);
-    var targetElement = inst.find('#output');
+    ngOnInit() {
 
-    if (!targetElement){
-      console.log('cant find the pivot element');
-      return;
-    }
-
-    //this helps if you build more than once as it will wipe the dom for that element
-    while (targetElement.firstChild){
-      targetElement.removeChild(targetElement.firstChild);
-    }
-    //here is the magic
-    console.log($.pivotUtilities);
-    targetElement.pivotUI(input,
-        {
-          rows: [],
-          cols: [],
-          rendererName: "Bar Chart",
-          renderers: $.extend(
-              $.pivotUtilities.renderers,
-              $.pivotUtilities.c3_renderers,
-              $.pivotUtilities.export_renderers
-          )
+        this.dataService.getUser().subscribe(data => {
+            var _dbs = this.dbs;
+            var _dbsModel = this.dbsModel;
+            data['server_configs'].forEach(function (server) {
+                _dbs.push(server);
+                if (_dbsModel['id'] == undefined) {
+                    _dbsModel = server;
+                }
+            });
+            console.log(data);
         });
-  }
+
+    }
+
+    renderTable(input) {
+
+        if (!this.el || !this.el.nativeElement || !this.el.nativeElement.children) {
+            console.log('cant build without element');
+            return;
+        }
+
+        var container = this.el.nativeElement;
+        var inst = jQuery(container);
+        var targetElement = inst.find('#output');
+
+        if (!targetElement) {
+            console.log('cant find the pivot element');
+            return;
+        }
+
+        //this helps if you build more than once as it will wipe the dom for that element
+        while (targetElement.firstChild) {
+            targetElement.removeChild(targetElement.firstChild);
+        }
+        //here is the magic
+        console.log($.pivotUtilities);
+        targetElement.pivotUI(input,
+            {
+                rows: [],
+                cols: [],
+                rendererName: "Bar Chart",
+                renderers: $.extend(
+                    $.pivotUtilities.renderers,
+                    $.pivotUtilities.c3_renderers,
+                    $.pivotUtilities.export_renderers
+                )
+            });
+    }
 
 
-  runQuery(event, mQuery, mColumns, mDb) {
-    console.log("query: " + mQuery);
-    const body = {query: {query: mQuery, server_id: mDb}, columnText : {TOKEN_COLUMNS : mColumns}};
-    this.dataService.runQuery(body).subscribe(data => {
-      console.log(data);
-      data['rows'].unshift(data['headings']);
-      this.renderTable(data['rows']);
-    });
-  }
+    runQuery(event, mQuery, mColumns, mDb) {
+        console.log("query: " + mQuery);
+        const body = {query: {query: mQuery, server_id: mDb}, columnText: {TOKEN_COLUMNS: mColumns}};
+        this.dataService.runQuery(body).subscribe(data => {
+            console.log(data);
+            data['rows'].unshift(data['headings']);
+            this.renderTable(data['rows']);
+        });
+    }
 
-  saveQuery(event, mQuery, mColumns, mDb) {
-    console.log("query: " + mQuery);
-    var queryText = {query : mQuery, columns: mColumns, serverId: mDb};
-    this.openQueryIdDialog(queryText);
-  }
+    saveQuery(event, mQuery, mColumns, mDb) {
+        console.log("query: " + mQuery);
+        var queryText = {query: mQuery, columns: mColumns, serverId: mDb};
+        this.openQueryIdDialog(queryText);
+    }
 
-  openQueryIdDialog(queryText): void {
-    let dialogRef = this.dialog.open(DialogComponent);
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
-    });
-  }
+    openQueryIdDialog(queryText):void {
+        this.dialog.open(DialogDataExampleDialog, {
+            data: {
+                animal: 'panda'
+            }
+        });
+    }
 }
 
 @Component({
-  selector: 'app-dialog',
-  templateUrl: 'dialog.component.html',
+    selector: 'mt-dialog-container',
+    templateUrl: 'save-query-dialog.html'
 })
-export class DialogComponent {
-  constructor(public dialogRef: MatDialogRef<DialogComponent>) {}
+export class DialogDataExampleDialog {
+    constructor(public data:any) {
+        console.log(data);
+    }
 }
